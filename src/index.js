c8de7856f49518eaaa2db73ff8e0ed0938a8f68a
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-05-07 17:57:53 -0500

    added static directory in express to server generated wintersmith site

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.get("/", (req, res) => {
   res.json({ heartbeat: "badum badum badum" });
 });
+
+// Docs live here!
+app.use("/docs", express.static(path.join(__dirname, "../docs/build")));
 
 module.exports = app;
