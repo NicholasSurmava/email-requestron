80e513daf20787ae9747bd902a5bbc39ae4e7bb9
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-05-07 19:36:08 -0500

    added an ejs index file and setup a page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/heartbeat", (req, res) => {
   res.json({ heartbeat: "badum badum badum" });
 });
 
